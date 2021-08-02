25bd7344979f2acfd4259d5b1ca0b71dc5942791
Celeste Dubini <celestedubini@hotmail.com>
2021-08-02 20:05:04 -0300

    Front: fixed various bugs from Register, post still doesn't work

--- client/src/Componets/Register/register.jsx
+++ client/src/Componets/Register/register.jsx
@@ -1,48 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './register.module.css';
 import { Link } from "react-router-dom";
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 function Register() {
     const [input, setInput] = useState({
-		fullname:'',
-		dni:'',
-        mail:'',
-		password:'',
-		birth_data:''
-	});
+        fullname: '',
+        dni: '',
+        mail: '',
+        password: '',
+        birth_data: ''
+    });
     const history = useHistory();
 
-    function handleChange(e){
+    function handleChange(e) {
         setInput({
             ...input,
             [e.target.name]: e.target.value
         })
     }
 
-    async function handleSubmit  (e){
+    async function handleSubmit(e) {
         e.preventDefault();
-        
-        try{
-                await fetch('http://localhost:3001/register',
-            {
-                method: 'POST',
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(input),
-            })
+        try {
+            await fetch('http://localhost:3001/register',
+                {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(input),
+                })
             alert('Account created succesfully!');
-        } catch(err){
+        } catch (err) {
             console.log(err.message)
             alert('We could not create account. Please try again.');
         }
 
         setInput({
-            fullname:'',
-		dni:'',
-        mail:'',
-		password:'',
-		birth_data:''
+            fullname: '',
+            dni: '',
+            mail: '',
+            password: '',
+            birth_data: ''
         })
         history.push('/home');
     }
@@ -51,36 +50,36 @@ function Register() {
     return (
         <div className="formulario">
             <h2> Create your Account </h2>
-        <form onSubmit={(e)=> handleSubmit(e)}>
-        <p>FullName</p>
-            <input type="text" placeholder="FullName"  id="FullName" required name='fullname' value={input.fullname} onChange={handleChange}/>
-        <p>Identification Number</p>
-            <input type="text" placeholder="Identification Number" id="IdentificationNumber" name='dni' required value={input.dni} onChange= {handleChange}/>
-        <p>E-mail</p>
-            <input type="text" placeholder="E-mail"  id="E-mail" required name='mail' value={input.mail} onChange= {handleChange}/>
-        <p>Password</p>
-            <input type="password" placeholder="Password" id="password" required  name='password' value={input.password} onChange= {handleChange}/>
-        <p>Repeat Password</p>
-            <input type="password" placeholder="Repeat Password" id="password" required name='password' value={input.password} onChange= {handleChange}/>
-            
-            <div className="form-row hide-inputbtns">
-        <label for="birthdate">Date of Birth</label>
-        <input className="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true" name='birth_data'  value={input.birth_data} onChange= {handleChange}/>
-        </div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <p>FullName</p>
+                <input type="text" placeholder="FullName" id="FullName" required name='fullname' value={input.fullname} onChange={handleChange} />
+                <p>Identification Number</p>
+                <input type="text" placeholder="Identification Number" id="IdentificationNumber" name='dni' required value={input.dni} onChange={handleChange} />
+                <p>E-mail</p>
+                <input type="text" placeholder="E-mail" id="E-mail" required name='mail' value={input.mail} onChange={handleChange} />
+                <p>Password</p>
+                <input type="password" placeholder="Password" id="password" required name='password' value={input.password} onChange={handleChange} />
+                <p>Repeat Password</p>
+                <input type="password" placeholder="Repeat Password" id="password" required name='password' value={input.password} onChange={handleChange} />
 
-        <div className="g-recaptcha" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+                <div className="form-row hide-inputbtns">
+                    <label for="birthdate">Date of Birth</label>
+                    <input className="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true" name='birth_data' value={input.birth_data} onChange={handleChange} />
+                </div>
 
-        <Link to='/home'> 
-            <button onclick="">Create User</button>
+                <div className="g-recaptcha" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+
+                <Link to='/home'>
+                    <button type="submit">Create User</button>
+                </Link>
+            </form>
+
+            <Link to='/recoverpassword'>
+                <p className='descriptionDetails'>Forgot Password</p>
             </Link>
-        </form>
-
-        <Link to='/recoverpassword'>
-            <p className='descriptionDetails'>Forgot Password</p>
-        </Link>
-        <Link to='/faq'>
-            <p className='descriptionDetails'>Frecuently Asked Questions</p>
-        </Link>
+            <Link to='/faq'>
+                <p className='descriptionDetails'>Frecuently Asked Questions</p>
+            </Link>
         </div>
     )
 }
