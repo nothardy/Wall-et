6dd404228b9270f5a173e89dbf7f84b2b3c59aff
Camilajure <camilajure@gmail.com>
2021-08-02 14:41:16 -0300

    client: component register advanced

--- client/src/Componets/Register/register.jsx
+++ client/src/Componets/Register/register.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './register.module.css';
+
 
 function register() {
     return (
-        <div>
+        <div class="formulario">
+             <h2> Create your Account </h2>
+        <form >
+        <p>FullName</p>
+            <input type="text" placeholder="FullName"  id="FullName" required value='' onChange= ''/>
+        <p>Identification Number</p>
+            <input type="text" placeholder="Identification Number" id="IdentificationNumber" required value='' onChange= ''/>
+        <p>E-mail</p>
+            <input type="text" placeholder="E-mail"  id="E-mail" required value='' onChange= ''/>
+        <p>Password</p>
+            <input type="password" placeholder="Password" id="password" required  value='' onChange= ''/>
+        <p>Repeat Password</p>
+            <input type="password" placeholder="Repeat Password" id="password" required value='' onChange= ''/>
             
+            <div class="form-row hide-inputbtns">
+        <label for="birthdate">Date of Birth</label>
+        <input class="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true"  value='' onChange= ''/>
+        </div>
+
+
+
+
+            <button onclick="">Create User</button>
+        </form>
         </div>
     )
 }
