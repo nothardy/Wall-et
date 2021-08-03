f0c682f1b9dbf2574483ac01f9b1c990f6111d48
Camilajure <camilajure@gmail.com>
2021-08-02 22:54:41 -0300

    client: genera nuevo usuario sin valdiaciones

--- client/src/Componets/Register/register.jsx
+++ client/src/Componets/Register/register.jsx
@@ -23,6 +23,7 @@ function Register() {
 
     async function handleSubmit(e) {
         e.preventDefault();
+                
         try {
             await fetch('http://localhost:3001/register',
                 {
@@ -31,19 +32,18 @@ function Register() {
                     body: JSON.stringify(input),
                 })
             alert('Account created succesfully!');
+            setInput({
+                fullname: '',
+                dni: '',
+                mail: '',
+                password: '',
+                birth_data: ''
+            })
+            history.push('/home');
         } catch (err) {
             console.log(err.message)
             alert('We could not create account. Please try again.');
         }
-
-        setInput({
-            fullname: '',
-            dni: '',
-            mail: '',
-            password: '',
-            birth_data: ''
-        })
-        history.push('/home');
     }
 
 
@@ -52,26 +52,26 @@ function Register() {
             <h2> Create your Account </h2>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <p>FullName</p>
-                <input type="text" placeholder="FullName" id="FullName" required name='fullname' value={input.fullname} onChange={handleChange} />
+                <input type="text" placeholder="FullName" id="title" required="required" name='fullname' value={input.fullname} onChange={handleChange} />
                 <p>Identification Number</p>
-                <input type="text" placeholder="Identification Number" id="IdentificationNumber" name='dni' required value={input.dni} onChange={handleChange} />
+                <input type="text" placeholder="Identification Number"  name='dni' required="required" value={input.dni} onChange={handleChange} />
                 <p>E-mail</p>
-                <input type="text" placeholder="E-mail" id="E-mail" required name='mail' value={input.mail} onChange={handleChange} />
+                <input type="text" placeholder="E-mail"  required="required" name='mail' value={input.mail} onChange={handleChange} />
                 <p>Password</p>
-                <input type="password" placeholder="Password" id="password" required name='password' value={input.password} onChange={handleChange} />
+                <input type="password" placeholder="Password" required="required" name='password' value={input.password} onChange={handleChange} />
                 <p>Repeat Password</p>
-                <input type="password" placeholder="Repeat Password" id="password" required name='password' value={input.password} onChange={handleChange} />
+                <input type="password" placeholder="Repeat Password"  required="required" name='password' value={input.password} onChange={handleChange} />
 
                 <div className="form-row hide-inputbtns">
                     <label for="birthdate">Date of Birth</label>
                     <input className="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true" name='birth_data' value={input.birth_data} onChange={handleChange} />
                 </div>
 
-                <div className="g-recaptcha" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+                {/* <div className="g-recaptcha" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div> */}
 
-                <Link to='/home'>
+                
                     <button type="submit">Create User</button>
-                </Link>
+               
             </form>
 
             <Link to='/recoverpassword'>
