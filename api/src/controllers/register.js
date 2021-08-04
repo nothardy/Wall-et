213203f69a231a6c1a59c0d169b201e21d49e69e
María Celeste Dubini <celestedubini@hotmail.com>
2021-08-04 11:21:17 -0300

    Back: password hashed

--- api/src/controllers/register.js
+++ api/src/controllers/register.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const { Account  } = require("../db");
-const { v4: uuidv4 } = require('uuid');;
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 
-function register(req, res, next) {
+async function register(req, res, next) {
     const { fullname , password, dni, mail, birth_date } = req.body;
-    if (!fullname || !password || !dni ||!mail || !birth_date) {throw new Error ("All fields are required ")};
+    try {
+      if (!fullname || !password || !dni ||!mail || !birth_date) {throw new Error ("All fields are required ")};
     if (password.length<8) {throw new Error ("Password it too short, should have 8 characters")};
-    Account.create({
+    const hashedPassword = await bcrypt.hash(password, 12);
+    await Account.create({
       id: uuidv4(),
       fullname,
-      password,
+      password: hashedPassword,
       dni,
-      mail,
+      mail: mail.toLowerCase(),
       birth_date
     })
     //   .then((recipeCreated) => {
@@ -22,8 +25,8 @@ function register(req, res, next) {
           message: "Created an Account succesfully",
         });
       })
-      .catch((error) => next(error));
-  }
+    } catch (error) {next(error)}
+}
 
   module.exports = {
     register
