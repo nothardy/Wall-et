1d23667ad5d14e34e22b29b08a4804d5b1a1fc15
María Celeste Dubini <celestedubini@hotmail.com>
2021-08-04 10:59:14 -0300

    Back: password limit added and changed returns

--- api/src/controllers/register.js
+++ api/src/controllers/register.js
@@ -4,7 +4,8 @@ const { v4: uuidv4 } = require('uuid');;
 
 function register(req, res, next) {
     const { fullname , password, dni, mail, birth_date } = req.body;
-    if (!fullname || !password || !dni ||!mail || !birth_date) return res.send({ error: 500, message: "All fields are required" });
+    if (!fullname || !password || !dni ||!mail || !birth_date) {throw new Error ("All fields are required ")};
+    if (password.length<8) {throw new Error ("Password it too short, should have 8 characters")};
     Account.create({
       id: uuidv4(),
       fullname,
