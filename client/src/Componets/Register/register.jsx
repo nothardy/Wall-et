970ca2107c0f19e9c35571dbd5ab628d5779ac8f
Camila Jure <camilajure@gmail.com>
2021-08-04 01:05:09 -0300

    miedo

--- client/src/Componets/Register/register.jsx
+++ client/src/Componets/Register/register.jsx
@@ -7,54 +7,54 @@ import ReCAPTCHA from "react-google-recaptcha";
 export function validate(input) {
     let errors = {};
     if (!input.mail) {
-      errors.mail = 'Se requiere un Email';
+      errors.mail = 'Required Email';
     } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.mail)) {
-      errors.mail = 'Email inválido';
+      errors.mail = 'Invalid Email ';
     }
     if (!input.password) {
-        errors.password = 'Se requiere una contraseña';
-      } else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(input.password)) {
-        errors.password = 'La contraseña debe contener ocho caracteres, al menos un caracter especial,una mayúscula y un número.';
+        errors.password = 'Required password';
+      } else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])/gm.test(input.password)) {
+        errors.password = 'The password must contain eight characters, an uppercase letter, and a number.';
       }
   
     return errors;
   };
 
 function Register() {
-    const [user, setUser] = useState({
+    const [input, setInput] = useState({
         fullname: '',
         dni: '',
         mail: '',
         password: '',
-        birth_date: ''
+        birth_data: ''
     });
     const [errors, setErrors] = useState({});
     const history = useHistory();
     const [captchaValido, cambiarCaptchaValido] = useState(null);
 	const [usuarioValido, cambiarUsuarioValido] = useState(false);
     const captcha= useRef(null);
-    const [loading, setLoading] = useState(false)
+    
 
     function handleChange(e) {
-        setUser({
-            ...user,
+                setInput({
+            ...input,
             [e.target.name]: e.target.value
         });
         setErrors(validate({
-            ...user,
-            [e.target.id]: e.target.value
+            ...input,
+            [e.target.value]: e.target.value
           }));
     }
 
     async function handleSubmit(e) {
         e.preventDefault();
-        setLoading(true)
+        
         if(captcha.current.getValue()){
-			console.log('El usuario no es un robot');
+			console.log('The user is not a robot');
 			cambiarUsuarioValido(true);
 			cambiarCaptchaValido(true);
 		} else {
-			console.log('Por favor acepta el captcha');
+			console.log('Please accept the captcha');
 			cambiarUsuarioValido(false);
 			cambiarCaptchaValido(false);
 		}
@@ -63,7 +63,7 @@ function Register() {
                 {
                     method: 'POST',
                     headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(user),
+                    body: JSON.stringify(input),
                 })
             alert('Account created succesfully!');
                 
@@ -76,7 +76,7 @@ function Register() {
 
     
 
-function onChange() {
+function captchaChange() {
     if(captcha.current.getValue()){
         console.log('The user is not a robot');
         cambiarCaptchaValido(true);
@@ -91,55 +91,63 @@ function onChange() {
             <div>
             <h2> Create your Account </h2>
             <form onSubmit={(e) => handleSubmit(e)}>
-                <p>FullName</p>
-                <input type="text" placeholder="FullName" id="title" required="required" name='fullname' value={user.fullname} onChange={handleChange} />
-                <p>Identification Number</p>
-                <input type="text" placeholder="Identification Number"  name='dni' required="required" value={user.dni} onChange={handleChange} />
-                <p>E-mail</p>
-                <input type="text" placeholder="E-mail"  required="required" name='mail' value={user.mail} onChange={handleChange} />
+                <p>FullName*</p>
+                <input type="text" placeholder="FullName" id="title" required="required" name='fullname' value={input.fullname} onChange={handleChange} />
+                <p>Identification Number*</p>
+                <input type="text" placeholder="Identification Number"  name='dni' required="required" value={input.dni} onChange={handleChange} />
+                
+                <div>
+                <p>E-mail*</p>
+                <input type="text" placeholder="E-mail"  required="required" name='mail' value={input.mail} onChange={handleChange} />
                 {errors.mail && (
                     <p className=''>{errors.mail}</p>
                     )} 
-                <p>Password</p>
-                <input type="password" placeholder="Password" required="required" name='password' value={user.password} onChange={handleChange} />
+                    </div>
+                    <div>
+                <p>Password*</p>
+                <input type="password" placeholder="Password" required="required" name='password' id="password" value={input.password} onChange={handleChange} />
                 {errors.password && (
                     <p className=''>{errors.password}</p>
                     )} 
-                <p>Repeat Password</p>
+                    </div>
+                    {/* <div>
+                <p>Repeat Password*</p>
                 <input type="password" placeholder="Repeat Password"  required="required" name="password2" id="password2" value={user.password} onChange={handleChange} />
                 {errors.password && (
                     <p className=''>{errors.password}</p>
                     )}
+                    </div> */}
                 <div className="form-row hide-inputbtns">
-                    <label for="birthdate">Date of Birth</label>
-                    <input className="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true" name='birth_date' value={user.birth_date} onChange={handleChange} />
+                    <label for="birthdata">Date of Birth*</label>
+                    <input className="birthdate" type="date" placeholder="YYYY-MM-DD" data-date-split-input="true" name='birth_data' value={input.birth_data} onChange={handleChange} required='required' min="1900-01-01" max="2003-12-31"/>
                 </div>
 
                 <div className="recaptcha">
                 <ReCAPTCHA
+                required
                     ref={captcha}
                     sitekey="6LfmRtcbAAAAAFHflEfIwe6cWeS89VI91LKYQ6sH"
-                    onChange={onChange}
+                    onChange={captchaChange}
                         />
                 </div>
 
             
-                
+                {captchaValido === false && <div className="error-captcha">Please accept the captcha</div>}
                     <button type="submit">Create User</button>
-                    {captchaValido === false && <div className="error-captcha">Por favor acepta el captcha</div>}
+                
                 
                 </form>
                 </div>
-}{usuarioValido && 
-				<div>
-					<h1>Welcome!</h1>
-				</div>
-			}
+}
             <Link to='/recoverpassword'>
                 <p className='descriptionDetails'>Forgot Password</p>
             </Link>
             <Link to='/faq'>
                 <p className='descriptionDetails'>Frecuently Asked Questions</p>
+            </Link>
+            <p>You already have an account?</p>
+            <Link to='/loging'>
+                <p className=''>Log in</p>
             </Link>
         </div>
                 
