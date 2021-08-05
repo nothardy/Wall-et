92d7f355d891590de7845e1330ee0008d6963fb3
MariaGeorgina <mgbaldassin@gmail.com>
2021-08-04 23:22:01 -0300

    loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-//import "./App.css";
->>>>>>> 216a3fcfb0ae4979824a9ae76ebb34fe8e61469f
+
 import { Route } from "react-router-dom";
 import Login from '../../client/src/Componets/Login/login';
 
