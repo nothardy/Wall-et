1dad03196cc2bba1e95c7b3cc6d55e89cc561a83
Gonzalo Juarez <75266150+Gonzaloejuarez@users.noreply.github.com>
2021-08-06 14:50:34 -0300

    (balance, home) : changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import { Route } from "react-router";
 import Balance from "./Components/Balance_2/Balance";
-import { Home } from "./Components/Home/Home_balance";
 function App() {
   return (
     <>
       <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/balance">
         <Balance />
       </Route>
     </>
