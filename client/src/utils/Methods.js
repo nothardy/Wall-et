20d9eea8d3ac18ad81fbea2f5cda4bf1157c1da4
Julian Piñel <julianpiniel@gmail.com>
2021-08-07 20:26:12 -0300

    (development): Method fixed, now is optimized and works properly

--- client/src/utils/Methods.js
+++ client/src/utils/Methods.js
@@ -1,9 +1,9 @@
-import moment from 'moment'
+import moment from "moment";
 
 export const setAxis = (transactions) => {
   // set x Axis
   let xAxis = [];
-  let today = moment().format('l');
+  let today = moment().format("l");
   for (let i = 0; i <= 4; i++) {
     let month = parseInt(today.charAt(0)) - i - 1;
     let xValue = new Date(2021, month, 1).toLocaleString("en-us", {
@@ -13,68 +13,9 @@ export const setAxis = (transactions) => {
   }
 
   // set y Axis
-  let yAxis = orderTransactions(transactions, xAxis).map(
-    (month) => month.amount
-  );
+  let yAxis = orderTransactions(transactions, xAxis);
 
   return [xAxis.reverse(), yAxis];
-};
-
-const orderTransactions = (transactions, xAxis) => {
-  let orderedTransactions = transactions.sort(
-    (transaction, nextTransaction) => {
-      if (
-        transaction.transaction_date.charAt(2) >
-        nextTransaction.transaction_date.charAt(2)
-      ) {
-        return 1;
-      } else if (
-        transaction.transaction_date.charAt(2) <
-        nextTransaction.transaction_date.charAt(2)
-      ) {
-        return -1;
-      } else return 0;
-    }
-  );
-
-  let monthTransactions = [];
-  for (let j = 0; j <= orderedTransactions.length - 1; j++) {
-    let totalAmount = 0;
-    let monthTransaction = {};
-    monthTransaction.amount = orderedTransactions[j].amount;
-    monthTransaction.month = orderedTransactions[j].transaction_date.charAt(2);
-    for (let i = 0; i <= orderedTransactions.length - 1; i++) {
-      if (
-        orderedTransactions[j].transaction_date.charAt(2) ===
-          orderedTransactions[i].transaction_date.charAt(2) &&
-        i !== j
-      ) {
-        totalAmount =
-          totalAmount +
-          orderedTransactions[j].amount +
-          orderedTransactions[i].amount;
-        monthTransaction.amount = totalAmount;
-      }
-      monthTransactions.push(monthTransaction);
-    }
-  }
-
-  let uniqueTransactions = [...new Set(monthTransactions)];
-
-  for (let i = 1; i <= uniqueTransactions.length - 1; i++) {
-    if (uniqueTransactions[i].month === uniqueTransactions[i - 1].month)
-      uniqueTransactions.splice(i, 1);
-  }
-
-  let months = xAxis
-    .map((month) => new Date(Date.parse(month + " 1, 2012")).getMonth() + 1)
-    .toString();
-
-  uniqueTransactions = uniqueTransactions.filter((transaction) =>
-    months.includes(transaction.month)
-  );
-
-  return uniqueTransactions;
 };
 
 export const getTransactionTypesPercentage = (transactions) => {
@@ -105,4 +46,36 @@ export const getTransactionTypesPercentage = (transactions) => {
 
   let transactionsPercentages = Object.values(transactionsInfo);
   return transactionsPercentages;
+};
+
+const orderTransactions = (transactions, xAxis) => {
+  let transactionFolder = {};
+  for (let i = 1; i <= 12; i++) {
+    transactionFolder[i] = transactions.filter(
+      (transaction) => parseInt(transaction.transaction_date.charAt(6)) === i
+    );
+  }
+
+  let totalTransactionAmounts = Object.values(transactionFolder).map(
+    (month) => {
+      let totalAmount = 0;
+      for (let transaction of month) {
+        if (transaction.amount) totalAmount = totalAmount + transaction.amount;
+        else totalAmount = 0;
+      }
+      return totalAmount;
+    }
+  );
+
+  let months = xAxis
+    .map((month) => new Date(Date.parse(month + " 1, 2012")).getMonth() + 1)
+    .toString();
+
+  totalTransactionAmounts = totalTransactionAmounts.filter((transaction) =>
+    months.includes(
+      totalTransactionAmounts.findIndex((month) => month === transaction) + 1
+    )
+  );
+
+  return totalTransactionAmounts;
 };
