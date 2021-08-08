822529870238d2f8f1a012bba7686dd1a3eaef28
María Celeste Dubini <celestedubini@hotmail.com>
2021-08-08 03:33:43 -0300

    Client/Front: changed color nav bar and added images to Working component

--- client/src/Components/Working/Working.jsx
+++ client/src/Components/Working/Working.jsx
@@ -5,6 +5,9 @@ function Working() {
         <div>
             <h3>We are developing something awesome for you</h3>
             <img src="https://mk0buildfireqbf86ll2.kinstacdn.com/wp-content/uploads/2020/07/how-long-does-it-take-to-build-a-mobile-app@3x-1024x487.png" alt="Working for you" />
+            <img src="https://media.giphy.com/media/cJHeQTbMltsjcCHiVs/giphy.gif" alt=" "></img>
+            {/* <img src="https://media.giphy.com/media/fTnSZ7b2eUEvyIOG6r/source.gif" alt=""></img>
+            <img src="https://media.giphy.com/media/YMjtdHfXclxz9guJjk/source.gif" alt=""></img> */}
         </div>
     )
 }
