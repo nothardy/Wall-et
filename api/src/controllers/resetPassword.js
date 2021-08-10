beb6676a53cdd31b7af065a6556d9c9131a1b586
Milagros Lechares <80794759+mflec@users.noreply.github.com>
2021-08-10 17:19:36 -0300

    conflicts resolved

--- api/src/controllers/resetPassword.js
+++ api/src/controllers/resetPassword.js
@@ -1,68 +1,5 @@
 require('dotenv').config();
 const { Account } = require("../db");
-<<<<<<< HEAD
-const bcrypt = require("bcrypt");
-const nodemailer = require("nodemailer");
-const { MAIL_ACCOUNT, MAIL_PASSWORD } = process.env;
-
-
-let resetCode
-
-const passwordReset = async (req, res) => {
-    const { mail } = req.body;
-
-    if (!mail) return res.status(404).send("The mail doesn't exists");
-    resetCode = Math.random().toString().slice(2, 7);
-
-    // To send an mail, we must first define a transporter. We will do it as follows:
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: MAIL_ACCOUNT,
-            pass: MAIL_PASSWORD,
-        },
-    }
-    );
-    // send mail with defined transport object
-    const info = {
-        from: MAIL_ACCOUNT, // sender address
-        to: mail, // receiver adress
-        subject: "Password Reset Request for Wall-et", //Subject mail
-        text: "Hi, " + user + ". Here's your code to reset your password: " + resetCode + ". If you did not request a new password, please ignore this mail.",
-    };
-
-    await transporter.sendMail(info, function (error, info) {
-        if (error) {
-            res.status(400).json({ message: "Sorry, something went wrong. Please try again later." });
-        } else {
-            res.status(200).json({ message: "Code sent" });
-        }
-    });
-
-};
-
-const resetVerificaction = async (req, res) => {
-    const { mail, reset_code, password } = req.body;
-    try {
-        const matchingCodes = (resetCode == reset_code)
-
-        switch (step) {
-            case "1":
-                if (matchingCodes) return res.status(200).json({ message: "Code accepted"});
-
-                if (!matchingCodes) return res.status(400).json({ message: "Code denied"});
-
-            case "2":
-                if (!password) return res.status(400).json({ message: "password cannot be null" });
-
-                const user = await Account.findOne({ where: { mail: mail } });
-                const hashedPassword = await bcrypt.hash(password, 12);
-                user.password = hashedPassword
-                await user.save();
-                return res.json({ message: "Password changed successfully", ok: true });
-
-        }
-=======
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
@@ -119,18 +56,14 @@ const resetVerificaction = async (req, res) => {
         user.password = hashedPassword
         await user.save();
         return res.json({ message: "Password changed successfully", ok: true });
->>>>>>> cb47043f5ecb3af855f3167110c6f881faa12bc6
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal server error" });
     }
 }
 
-<<<<<<< HEAD
-module.exports= {
-=======
+
 module.exports = {
->>>>>>> cb47043f5ecb3af855f3167110c6f881faa12bc6
     passwordReset,
     resetVerificaction
 }
