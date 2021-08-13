51fdadf01fff7afd70a9df4d4ba30dc6405e4bbe
MariaGeorgina <mgbaldassin@gmail.com>
2021-08-13 08:58:49 -0300

    help client

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 import Home from "./Components/Home/index";
+import Help from "./Components/Help/help";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/t"><Home/></Route> 
+      <Route exact path='/help' component={Help}></Route>
     </div>
   );
 }
---
+++ client/src/Redux/Actions/helpActions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import swal from 'sweetalert';
+
+const USER_HELP_REQUEST = 'USER_HELP_REQUEST';
+const USER_HELP_SUCCESS = 'USER_HELP_SUCCESS';
+
+axios.defaults.baseURL = 'http://localhost:3001/';
+
+export const help = ({ mail, mailSubject, mailBody }) => (dispatch) => {
+    dispatch({ type: USER_HELP_REQUEST, payload: { mail, mailSubject, mailBody }});
+   return axios.post('/help', { mail, mailSubject, mailBody })
+   .then(response => {
+     dispatch({ type: USER_HELP_SUCCESS, payload: response.data });
+     swal('Message sent', { icon : 'success' })
+    })
+   
+   .catch(error => swal('Maintenance problems', { icon: 'warning'}));
+};
+
