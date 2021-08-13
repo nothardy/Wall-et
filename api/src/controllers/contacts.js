00d8678e199fce6e3795ac7022eaca67a2fe56cb
Julian Piñel <julianpiniel@gmail.com>
2021-08-13 00:16:23 -0300

    filters and searchbar working, back working but with no transactions to test :)

--- api/src/controllers/contacts.js
+++ api/src/controllers/contacts.js
@@ -2,49 +2,69 @@ const { Account, Transaction, Card, Contact } = require("../db");
 const { Op } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
 
-const getContactsFromDb = async (req, res, next) => {
-  if (req.url.includes("?email")) {
-    try {
-      const searchedContact = req.query.email;
-      const contacts = await Account.findAll({
-        // chequear de donde la traigo
-        where: {
-          mail: { [Op.iLike]: `%${searchedContact}%` },
+// const getContactsFromDb = async (req, res, next) => {
+//   if (req.url.includes("?email")) {
+//     try {
+//       const searchedContact = req.query.email;
+//       const contacts = await Account.findAll({
+//         // chequear de donde la traigo
+//         where: {
+//           mail: { [Op.iLike]: `%${searchedContact}%` },
+//         },
+//         include: [
+//           {
+//             model: Contact,
+//             as: "contacts",
+//             attributes: ["id", "mail", "cvu"],
+//             through: {
+//               attributes: [],
+//             },
+//           },
+//         ],
+//       });
+//       if (contacts.length > 0) return res.json(contacts);
+//     } catch (error) {
+//       next(error);
+//     }
+//   } else {
+//     try {
+//       const contacts = await Account.findAll({
+//         include: [
+//           {
+//             model: Contact,
+//             as: "contacts",
+//             attributes: ["id", "mail", "cvu"],
+//             through: {
+//               attributes: [],
+//             },
+//           },
+//         ],
+//       });
+//       if (contacts.length > 0) return res.json(contacts);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// };
+
+const getTransactions = async (req, res) => {
+  const { id } = req.body;
+  const transactions = await Account.findOne({
+    where: {
+      id: id,
+    },
+    include: [
+      {
+        model: Transaction,
+        as: "transactions",
+        attributes: ["to", "createdAt"],
+        through: {
+          attributes: [],
         },
-        include: [
-          {
-            model: Contact,
-            as: "contacts",
-            attributes: ["id", "mail", "cvu"],
-            through: {
-              attributes: [],
-            },
-          },
-        ],
-      });
-      if (contacts.length > 0) return res.json(contacts);
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    try {
-      const contacts = await Account.findAll({
-        include: [
-          {
-            model: Contact,
-            as: "contacts",
-            attributes: ["id", "mail", "cvu"],
-            through: {
-              attributes: [],
-            },
-          },
-        ],
-      });
-      if (contacts.length > 0) return res.json(contacts);
-    } catch (error) {
-      next(error);
-    }
-  }
+      },
+    ],
+  });
+  res.json(transactions);
 };
 
 const addContactToDb = async (req, res, next) => {
@@ -68,4 +88,4 @@ const addContactToDb = async (req, res, next) => {
   }
 };
 
-module.exports = { getContactsFromDb, addContactToDb };
+module.exports = { getTransactions, addContactToDb };
