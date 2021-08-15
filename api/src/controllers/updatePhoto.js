6675a7eb028bdb7650e0587a2a3e8edaafa125f9
María Celeste Dubini <celestedubini@hotmail.com>
2021-08-15 04:35:00 -0300

    Api/Back: works the upload photo but on the front doesn't renderize

--- api/src/controllers/updatePhoto.js
+++ api/src/controllers/updatePhoto.js
@@ -1,6 +1,19 @@
-const updatePhoto = (req, res) => {
-    console.log(req.file)
-    res.send('uploaded')
+const { Account } = require("../db");
+
+const updatePhoto = async (req, res, next) => {
+    const id = req.body.id; 
+    const photo = req.file.path;
+    console.log(photo)
+    try {
+        await Account.update({photo: photo},
+            {where: 
+                {id: id}
+            });
+        return res.json({message: true}).status(200);
+    } catch (error) {
+        next(error);
+        return res.json(error);
+    };
 }
 
 module.exports = {
--- api/src/middlewares/multer.js
+++ api/src/middlewares/multer.js
@@ -1,16 +1,27 @@
 const multer = require("multer")
 const path = require("path")
+const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "../public/uploads"),
+    destination: path.join(__dirname, "../uploads"),
     filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase())
     }
 })
 
 const multerMiddleware = multer({
     storage: storage,
-    dest: path.join(__dirname, "../public/uploads")
+    dest: path.join(__dirname, "../public/uploads"),
+    limits: {fileSize: 1000000},
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png|gif/;
+        const mimetype = fileTypes.test(file.mimetype);
+        const extname = fileTypes.test(path.extname(file.originalname));
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+        cb("Error: file must be an image and should be a jpeg, jpg, png or a gif.")
+    }
 }).single("photo")
 
 module.exports = {
