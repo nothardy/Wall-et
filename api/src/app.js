746350e93a33037ff80d3d8fb8e237cee580b648
Julian Piñel <julianpiniel@gmail.com>
2021-08-17 09:41:41 -0300

    attempt to fix CORS error

--- api/src/app.js
+++ api/src/app.js
@@ -13,16 +13,16 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  //res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   //res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use(cors());
 
