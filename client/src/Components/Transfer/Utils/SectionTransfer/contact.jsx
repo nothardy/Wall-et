a3d3a386ffb07fe1503d76646cc318c215c097e6
Julian Piñel <julianpiniel@gmail.com>
2021-08-17 02:43:18 -0300

    fixed parenthesis for deploy

--- client/src/Components/Transfer/Utils/SectionTransfer/contact.jsx
+++ client/src/Components/Transfer/Utils/SectionTransfer/contact.jsx
@@ -6,7 +6,6 @@ import c from "./contact.module.css";
 const Contact = ({ toggleTransaction }) => {
   const contacts = useSelector((state) => state.contactsReducer.contacts);
 
-   
   return (
     <div className={c.container}>
       <div className={c.headerContact}>
@@ -16,21 +15,28 @@ const Contact = ({ toggleTransaction }) => {
       <div className={c.bodyContact}>
         {
           /* ACA TENDRIA QUE CONECTAR CON MI STORE. */
-          contacts ? contacts.map((el) => (
-            <ViewContact
-              key={el.id}
-              id={el.id}
-              CVU={el.CVU}
-              fullname={el.fullname}
-              mail={el.mail}
-              toggleTransaction={toggleTransaction}
-            />):<img src="https://image.flaticon.com/icons/png/512/3565/3565856.png" alt="tda" />
-           /* PONER EL CASO DE QUE NO ALLA CONTACT, APAREZCA UNA IMG */
+          contacts ? (
+            contacts.map((el) => (
+              <ViewContact
+                key={el.id}
+                id={el.id}
+                CVU={el.CVU}
+                fullname={el.fullname}
+                mail={el.mail}
+                toggleTransaction={toggleTransaction}
+              />
+            ))
+          ) : (
+            <img
+              src="https://image.flaticon.com/icons/png/512/3565/3565856.png"
+              alt="tda"
+            />
+          )
+          /* PONER EL CASO DE QUE NO ALLA CONTACT, APAREZCA UNA IMG */
         }
       </div>
     </div>
   );
 };
-
 
 export default Contact;
