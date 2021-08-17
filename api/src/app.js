98241339ea4ada52bdb3288f5d9793c4b9f45e53
Julian Piñel <julianpiniel@gmail.com>
2021-08-17 09:53:44 -0300

    attempt number 100000 to fix cors error

--- api/src/app.js
+++ api/src/app.js
@@ -17,18 +17,16 @@ server.use(
     origin: "*",
   })
 );
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
-
-server.use(cors());
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/", routes);
 
