a06c0edfe85cd8d7b858db6b680186a725ff3976
Julian Piñel <julianpiniel@gmail.com>
2021-08-17 09:49:18 -0300

    fixed cors error

--- api/src/app.js
+++ api/src/app.js
@@ -12,7 +12,11 @@ server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json());
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use(cors());
+server.use(
+  cors({
+    origin: "*",
+  })
+);
 // server.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 //   res.header("Access-Control-Allow-Credentials", "true");
@@ -20,7 +24,7 @@ server.use(cors());
 //     "Access-Control-Allow-Headers",
 //     "Origin, X-Requested-With, Content-Type, Accept"
 //   );
-//   //res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 //   next();
 // });
 
