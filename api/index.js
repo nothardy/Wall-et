bc6a3b051c4cc0ef4190172ca23c5542e1af2d65
Julian Piñel <julianpiniel@gmail.com>
2021-08-24 14:06:26 -0300

    force false

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 	server.listen(process.env.PORT, () => {
 		console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 	});
