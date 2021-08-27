1fd067ca11b4261e30209f66203170baa1117a2e
Julian Piñel <julianpiniel@gmail.com>
2021-08-27 06:41:06 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-
+//deploy
 conn.sync({ force: false }).then(() => {
 	server.listen(process.env.PORT, () => {
 		console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
